f80f2e172ceea1da13fd710cb67ba2c6b610c1c5
TomaszLukasik007 <Vampirektransinc@gmail.com>
2020-11-11 14:42:05 -0600

    First Commit1

--- src/Office_Hours/Practice_11_11_2020/ArrayPractice1.java
+++ src/Office_Hours/Practice_11_11_2020/ArrayPractice1.java
@@ -14,16 +14,33 @@ public class ArrayPractice1 {
   MUST use for each loops
 
          */
+        int[] arr1 = {1,2,3,4,5,6,7,8,9};
 
-     int[] arr1 = {1,2,3,4,5};
-     int[] arr2 = {4,5,6,7,8};
+        int[] arr2 = {10,11,12,13,14,15,1};
 
+        for(int j = 0; j <= arr1.length -1; j++){ // iterates the arr1' indexes
+            int element = arr1[j];  //1
+            for(int i =0; i <= arr2.length-1; i++){ // iterates the arr2' indexes
+                if(element == arr2[i] ){
+                    System.out.println(element);
+                }
+            }
+        }
 
+        System.out.println("=============================================");
 
+        for(int element : arr1 ){
 
+            for(int each: arr2){
+                if(each == element){
+                    System.out.println(element);
+                }
+            }
 
-
+        }
 
 
     }
+
 }
+
---
+++ src/Office_Hours/Practice_11_11_2020/ArraysPractice3.java
@@ -0,0 +1,14 @@
+package Office_Hours.Practice_11_11_2020;
+
+public class ArraysPractice3 {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+    }
+}
