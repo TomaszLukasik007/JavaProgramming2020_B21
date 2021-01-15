4316835ab43815b323805d4d54006ac4f2d619a9
TomaszLukasik007 <Vampirektransinc@gmail.com>
2021-01-15 13:19:03 -0600

    Adding Library and New Classes8

---
+++ src/day49_Abstraction/ShapeTask/Shape.java
@@ -0,0 +1,28 @@
+package day49_Abstraction.ShapeTask;
+
+public abstract class Shape {
+
+    public final String name;
+
+    public Shape(String name) {
+        if(name.isEmpty()){
+            throw new RuntimeException("Name can not be empty");
+        }
+        this.name = name;
+    }
+
+    public abstract double area();
+    public abstract double perimeter();
+
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "name= '" + name + '\'' +
+                ", area= '" + area() + '\'' +
+                ", perimeter= '" + perimeter() + '\'' +
+                '}';
+    }
+
+
+}
