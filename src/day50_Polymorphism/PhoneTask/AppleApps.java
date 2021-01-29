02f57985360a294eed07d0567acc7c7d880de2a8
TomaszLukasik007 <Vampirektransinc@gmail.com>
2021-01-28 19:44:37 -0600

    Adding Library and New Classes13

--- src/day50_Polymorphism/PhoneTask/AppleApps.java
+++
@@ -1,7 +0,0 @@
-package day50_Polymorphism.PhoneTask;
-
-public interface AppleApps  extends Downloadable{
-        String AppStoreName = "Apple";
-        String OS = "iOS 14";
-
-}
--- src/day50_Polymorphism/PhoneTask/Downloadable.java
+++
@@ -1,7 +0,0 @@
-package day50_Polymorphism.PhoneTask;
-
-public interface Downloadable {
-
-    void downloadApp();
-
-}
